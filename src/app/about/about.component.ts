9d9b84b7d78522523fd5cacc14445ab54d8092c2
Bastien Lemaire <bastien.lemaire@gmail.com>
2019-03-27 20:20:52 +0100

    Concat

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {interval, noop, of} from 'rxjs';
+import {concat} from 'rxjs/internal/observable/concat';
+import {take} from 'rxjs/operators';
 
 @Component({
   selector: 'about',
@@ -11,6 +14,17 @@ export class AboutComponent implements OnInit {
   }
 
   ngOnInit() {
+    const source1$ = interval(1000).pipe(take(3));
+    const source2$ = of(4, 5, 6);
+    const source3$ = of(7, 8, 9);
+
+    const results$ = concat(source1$, source2$, source3$);
+
+    results$.subscribe(
+      value => console.log(value),
+      noop,
+      () => console.log('completed')
+    );
   }
 
 }
