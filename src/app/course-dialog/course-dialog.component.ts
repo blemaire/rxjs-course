7d29ba521dd3cab0baf9f4177a43fc3f93eeda7a
Bastien Lemaire <bastien.lemaire@gmail.com>
2019-03-27 21:20:03 +0100

    exhaustMap

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -62,8 +62,9 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
   }
 
     ngAfterViewInit() {
-
-
+      fromEvent(this.saveButton.nativeElement, 'click').pipe(
+        exhaustMap(() => this.saveCourse(this.form.value))
+      ).subscribe();
     }
 
 
