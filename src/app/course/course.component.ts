25c309e2ebdd5d163b17e8d22c754af612a6a5b8
Bastien Lemaire <bastien.lemaire@gmail.com>
2019-04-03 22:15:05 +0200

    Course component no uses the store

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -16,6 +16,7 @@ import {
 import { merge, fromEvent, Observable, concat } from 'rxjs';
 import { Lesson } from '../model/lesson';
 import { createHttpObservable } from '../common/util';
+import { Store } from '../common/store.service';
 
 
 @Component({
@@ -25,19 +26,22 @@ import { createHttpObservable } from '../common/util';
 })
 export class CourseComponent implements OnInit, AfterViewInit {
 
-    courseId: string;
+    courseId: number;
     course$: Observable<Course>;
     lessons$: Observable<Lesson[]>;
 
     @ViewChild('searchInput') input: ElementRef;
 
-    constructor(private route: ActivatedRoute) {
+    constructor(
+        private route: ActivatedRoute,
+        private store: Store
+    ) {
 
     }
 
     ngOnInit() {
         this.courseId = this.route.snapshot.params['id'];
-        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+        this.course$ = this.store.selectCourseById(this.courseId);
     }
 
     ngAfterViewInit() {
